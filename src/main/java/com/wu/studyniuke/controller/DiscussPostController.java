3faad8dc45971629045cee549ed36e2588418730
wym <1205271990@qq.com>
2021-05-27 09:34:49 +0800

    version2.4  实现帖子详情。

--- src/main/java/com/wu/studyniuke/controller/DiscussPostController.java
+++ src/main/java/com/wu/studyniuke/controller/DiscussPostController.java
@@ -3,10 +3,13 @@ package com.wu.studyniuke.controller;
 import com.wu.studyniuke.entity.DiscussPost;
 import com.wu.studyniuke.entity.User;
 import com.wu.studyniuke.service.DiscussPostService;
+import com.wu.studyniuke.service.UserService;
 import com.wu.studyniuke.util.CommunityUtil;
 import com.wu.studyniuke.util.HostHolder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -26,12 +29,18 @@ public class DiscussPostController {
     @Autowired
     private HostHolder hostHolder;
 
+    @Autowired
+    private UserService userService;
+
     @RequestMapping(path = "/add",method = RequestMethod.POST)
     @ResponseBody
     public String addDiscussPost(String title,String content){
         User user =hostHolder.getUser();
         if(user==null){
             return CommunityUtil.getJSONString(403,"unLogin");
+        }
+        if(title==null||content==null){
+            return CommunityUtil.getJSONString(403,"不能为空");
         }
         DiscussPost discussPost = new DiscussPost();
         discussPost.setTitle(title);
@@ -41,6 +50,16 @@ public class DiscussPostController {
 
         discussPostService.addDiscussPost(discussPost);
         return CommunityUtil.getJSONString(0,"success");
+
+    }
+
+    @RequestMapping(path="/detail/{discussId}",method = RequestMethod.GET)
+    public String showDiscussPost(@PathVariable("discussId") int discussId, Model model){
+        DiscussPost post = discussPostService.queryDiscussPost(discussId);
+        model.addAttribute("post",post);
+        User user = userService.findUserById(post.getUserId());
+        model.addAttribute("user",user);
+        return "site/discuss-detail";
 
     }
 
